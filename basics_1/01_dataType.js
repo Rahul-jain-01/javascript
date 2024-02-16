53aab060b5d4c8cd3443cc7a8f27165be47f22b0
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2024-02-16 14:12:28 +0000

    datatype learnt

---
+++ basics_1/01_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3); // It wont work because we are using node.js, not browser
+
+let id=117;
+let name="Rahul";
+let age_18=true;
+let city;
+
+//number
+//bigint
+//string => ""
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
