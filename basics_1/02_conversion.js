ffeee138fb679bdd5a198168e4166969c2eaf377
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2024-02-16 14:35:17 +0000

    conversion done

---
+++ basics_1/02_conversion.js
@@ -0,0 +1,26 @@
+let score=true;
+
+//console.log(typeof(score));
+
+let valueInNumber=Number(score); //use datatype in CAPs like Number, String, Boolean
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 =>false;
+//"" => false
+//"rahul" => true
+
+let someNumber=35;
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
