179567ca1100ad199930151b7040b2f92acc6c85
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2024-02-18 14:59:12 +0000

    summary done

--- basics_1/02_conversion.js
+++ basics_1/02_conversion.js
@@ -24,3 +24,5 @@ let someNumber=35;
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+ 
---
+++ basics_1/03_dataTypes_summary.js
@@ -0,0 +1,21 @@
+//Data types is categorized by  how the elements are accessed and referenced
+
+//Primitive
+
+// 7 types : String, number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference Type(Non primitive)
+
+// Array, Objects, Functions 
+
+const heroes=["cap","ironman","thor"] //type => object
+let myObj={
+    name:"Rahul",
+    age: 22,
+}                       //type => object
+
+const myFunction=function(){        //type => object function
+    console.log("Hello world")
+}
+
+//Note: Null has type object
