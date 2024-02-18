2c654f8f1e7f17fd133d75e71e4bf7b71d5ff1b4
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2024-02-18 17:05:49 +0000

    String operation

---
+++ basics_1/04_strings.js
@@ -0,0 +1,20 @@
+let name="Rahul";
+const repoCount=50;
+
+//console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('Rahul-jain-hv');
+
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.substring(0,3));
+// console.log(gameName.slice(-7,3));
+
+const url="https://rahul.com/rahul%01jain"
+
+// console.log(url.replace('%01','.'));
+// console.log(url.includes('rahul'));
+
+console.log(gameName.split('-'));
