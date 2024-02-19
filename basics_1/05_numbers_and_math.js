a0dce3edafc3be51e0acd9c8b687384282fb1484
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2024-02-19 07:43:50 +0000

    math operations

---
+++ basics_1/05_numbers_and_math.js
@@ -0,0 +1,24 @@
+const score=400;
+//console.log(score);
+
+const balance=new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+
+const fixedNumber=123.8666
+// console.log(fixedNumber.toPrecision(1));
+ 
+const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-5)); //changes negative sign to positive sign
+
+// console.log(Math.round(4.6));
+
+console.log(Math.random()); //random() gives values between 0 and 1
